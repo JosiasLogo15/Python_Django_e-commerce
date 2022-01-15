1f27a2e0b7b7b0fc7246caa39406787aceacaf45
JosiasLogo15 <josiasmaldonado2002@gmail.com>
2022-01-15 01:02:48 -0400

    Adicionando debug toolbar

--- pystore/urls.py
+++ pystore/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +12,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    #urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
